92c933fb8c647f99d38bf8c8e138eebb772a70b5
Anil Dukkipatty <anildukkipatty@gmail.com>
2018-09-08 20:07:31 +0530

    WIP

--- peer/nse.js
+++ peer/nse.js
@@ -1,15 +1,19 @@
-const app = require('express')()
+const express = require('express')
+const app = express()
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
+const path = require('path')
 const { post } = require('../lib/http')
 const { getLatestTx, getLedger, insertToLedger } = require('./lib')
+
+app.use(express.static(path.resolve('html/public')))
 
 app.get('/', renderIndex)
 app.get('/syncLedger', syncLedger)
 app.get('/showLedger', showLedger)
 
 function renderIndex (req, res) {
-  res.send('PEER1 running')
+  res.sendFile(path.resolve('html/nse.html'))
 }
 function syncLedger (req, res) {
   post('http://localhost:9999/syncPeer', { latestHeight: getLatestTx().height })

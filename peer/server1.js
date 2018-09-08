fae6ec54b3a7658cae8f29dac2816a2efb3a7933
Anil Dukkipatty <anildukkipatty@gmail.com>
2018-09-08 18:12:52 +0530

    WIP

--- peer/server1.js
+++ peer/server1.js
@@ -1,7 +1,8 @@
-const express = require('express')
+const app = require('express')()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
 const { post } = require('../lib/http')
 const { getLatestTx, getLedger, insertToLedger } = require('./lib')
-const app = express()
 
 app.get('/', renderIndex)
 app.get('/syncLedger', syncLedger)
@@ -14,6 +15,7 @@ function syncLedger (req, res) {
   post('http://localhost:9999/syncPeer', { latestHeight: getLatestTx().height })
   .fork(err => {}, transactions => {
     insertToLedger(transactions)
+    io.emit('data', transactions)
     return res.send('OK')
   })
 }
@@ -21,4 +23,4 @@ function showLedger (req, res) {
   return res.send(getLedger())
 }
 
-app.listen('4000')
+server.listen('4000')
